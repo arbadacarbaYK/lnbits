897e5b2102dbcc8ccf8b848b16aa988e36b656f0
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2024-05-16 12:26:38 +0200

    void errormsg when lnbits doesnt run on onion/https
    if served over http on localhost In Server you see the funding source is successfully connected but the errormsg saiys funding source is void wallet which might be confusing, so i expanded the err msg.

--- lnbits/wallets/void.py
+++ lnbits/wallets/void.py
@@ -15,7 +15,7 @@ from .base import (
 class VoidWallet(Wallet):
     async def create_invoice(self, *_, **__) -> InvoiceResponse:
         return InvoiceResponse(
-            ok=False, error_message="VoidWallet cannot create invoices."
+            ok=False, error_message="VoidWallet cannot create invoices. Please check funding source config and make sure LNbits is publicly accessible. "
         )
 
     async def status(self) -> StatusResponse:
